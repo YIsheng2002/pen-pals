b4f617e185ed11ae4c2eb0be5cee22cf14cd9ca2
yi sheng <shengyiheng2002@gmail.com>
2023-12-28 01:12:35 +0800

    simple set up

---
+++ pen-pals/src/App.java
@@ -0,0 +1,8 @@
+import com.pen_pals.db.MyDatabase;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        MyDatabase.doConnection();
+    }
+}
---
+++ pen-pals/src/com/pen_pals/db/MyDatabase.java
@@ -0,0 +1,29 @@
+package com.pen_pals.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.Properties;
+
+public class MyDatabase {
+    static  Connection conn = null;
+
+    public static Connection doConnection() throws ClassNotFoundException, SQLException
+    {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+
+        return conn;
+    }
+
+    public static void main(String[] args){
+        try{
+            System.out.println(MyDatabase.doConnection());
+        } catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
