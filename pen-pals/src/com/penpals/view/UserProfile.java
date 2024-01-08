9aa28965a83f5d10265f9fd0ccb1c02d76e79240
p0205 <kks741224@gmail.com>
2024-01-08 12:14:16 +0800

    User profile

--- pen-pals/src/com/penpals/view/UserProfile.java
+++ pen-pals/src/com/penpals/view/UserProfile.java
@@ -27,80 +27,98 @@ import java.awt.Color;
 public class UserProfile extends JFrame implements ActionListener, MouseListener{
 
 	private static final long serialVersionUID = 1L;
+	
+	//center Panel
 	private JPanel contentPane;
-	private JPanel textPanel;
-	private JPanel fieldLabelPanel;
-	private JLabel usernameLabel;
-	private JLabel passwordLabel;
-	private JLabel nameLabel;
-	private JLabel telNumberLabel;
-	private JLabel emailLabel;
-	private JLabel addressLabel;
-	private JLabel usernameFieldLabel;
-	private JLabel passwordFieldLabel;
-	private JLabel nameFieldLabel;
-	private JLabel telNumberFieldLabel;
-	private JLabel emailFieldLabel;
-	private JLabel addressNumberFieldLabel;
-	private JLabel addressRoadFieldLabel;
-	private JLabel addressPostcodeFieldLabel;
-	private JLabel addressStateFieldLabel;
-	private Dimension frameSize;
-	private JLabel userImage;
-	private JButton backButton;
+		//page start panel
+		private JLabel userImage;
+		//line start panel
+		private JPanel textPanel;
+			private JLabel usernameLabel;
+			private JLabel passwordLabel;
+			private JLabel nameLabel;
+			private JLabel telNumberLabel;
+			private JLabel emailLabel;
+			private JLabel addressLabel;
+			
+		private JPanel fieldLabelPanel;
+			private JLabel usernameFieldLabel;
+			private JLabel passwordFieldLabel;
+			private JLabel nameFieldLabel;
+			private JLabel telNumberFieldLabel;
+			private JLabel emailFieldLabel;
+			private JLabel addressNumberFieldLabel;
+			private JLabel addressRoadFieldLabel;
+			private JLabel addressPostcodeFieldLabel;
+			private JLabel addressStateFieldLabel;
+	
+	//north Panel
+	private JPanel northPanel;
+		private JButton backButton;
+			private ImageIcon backIcon;
 	/**
 	 * Create the frame.
 	 */
 	public UserProfile () {
+		init();
+	}
+		
+		
+	public void init() {
 		 //Frame
 		setBounds(100, 100, 450, 550);
-				setTitle("User Profile");
-				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frameSize = new Dimension(450, 550); // Adjust the dimensions as needed
-		        setMinimumSize(frameSize);
-		        setLayout(new BorderLayout());
-		//set user image
-        userImage = new JLabel();
-        ImageIcon image = createResizedIcon("/resources/uiSymbol/user_profile.png",50,50);
-        userImage.setIcon(image);
-        userImage.setHorizontalAlignment(JLabel.CENTER);
-        userImage.setVerticalAlignment(JLabel.BOTTOM);
+		
+		setTitle("Penpal Gift Shop");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(450, 550));
+        setLayout(new BorderLayout());
         
+        contentPane = new JPanel(new BorderLayout(5,5));
+        contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));  
+        	
+			//set user image
+	        userImage = new JLabel();
+	        ImageIcon image = createResizedIcon("/resources/uiSymbol/user_profile.png",50,50);
+	        userImage.setIcon(image);
+	        userImage.setHorizontalAlignment(JLabel.CENTER);
+	        userImage.setVerticalAlignment(JLabel.BOTTOM);
+	        
+	    contentPane.add(userImage,BorderLayout.PAGE_START);
 		        
-				// label
-				usernameLabel = new JLabel();
-				usernameLabel.setText("Username");
-				usernameLabel.setPreferredSize(new Dimension(200,30));
-				usernameLabel.setHorizontalAlignment(4);
-				
-				passwordLabel = new JLabel();
-				passwordLabel.setText("Password");
-				passwordLabel.setPreferredSize(new Dimension(200,30));
-				passwordLabel.setHorizontalAlignment(4);
-				
-				nameLabel = new JLabel();
-				nameLabel.setText("Full Name");
-				nameLabel.setPreferredSize(new Dimension(200,30));
-				nameLabel.setHorizontalAlignment(4);
-				
-				telNumberLabel = new JLabel();
-				telNumberLabel.setText("Telephone Number");
-				telNumberLabel.setPreferredSize(new Dimension(200,30));
-				telNumberLabel.setHorizontalAlignment(4);
-				
-				emailLabel = new JLabel();
-				emailLabel.setText("Email");
-				emailLabel.setPreferredSize(new Dimension(200,30));
-				emailLabel.setHorizontalAlignment(4);
-				
-				addressLabel = new JLabel();
-				addressLabel.setText("Address");
-				addressLabel.setPreferredSize(new Dimension(200,30));
-				addressLabel.setHorizontalAlignment(4);
-				
-				//creation of a panel to contain Jlabels
+			  //creation of a panel to contain Jlabels
 				textPanel = new JPanel();
 				textPanel.setPreferredSize(new Dimension(200,250));
+					// label
+					usernameLabel = new JLabel();
+					usernameLabel.setText("Username");
+					usernameLabel.setPreferredSize(new Dimension(200,30));
+					usernameLabel.setHorizontalAlignment(4);
+					
+					passwordLabel = new JLabel();
+					passwordLabel.setText("Password");
+					passwordLabel.setPreferredSize(new Dimension(200,30));
+					passwordLabel.setHorizontalAlignment(4);
+					
+					nameLabel = new JLabel();
+					nameLabel.setText("Full Name");
+					nameLabel.setPreferredSize(new Dimension(200,30));
+					nameLabel.setHorizontalAlignment(4);
+					
+					telNumberLabel = new JLabel();
+					telNumberLabel.setText("Telephone Number");
+					telNumberLabel.setPreferredSize(new Dimension(200,30));
+					telNumberLabel.setHorizontalAlignment(4);
+					
+					emailLabel = new JLabel();
+					emailLabel.setText("Email");
+					emailLabel.setPreferredSize(new Dimension(200,30));
+					emailLabel.setHorizontalAlignment(4);
+					
+					addressLabel = new JLabel();
+					addressLabel.setText("Address");
+					addressLabel.setPreferredSize(new Dimension(200,30));
+					addressLabel.setHorizontalAlignment(4);
+				
 				textPanel.add(usernameLabel);
 				textPanel.add(passwordLabel);
 				textPanel.add(nameLabel);
@@ -108,56 +126,59 @@ public class UserProfile extends JFrame implements ActionListener, MouseListener
 				textPanel.add(emailLabel);
 				textPanel.add(addressLabel);
 				
-				//textfield Label
-				usernameFieldLabel = new JLabel("old username");
-				usernameFieldLabel.setForeground(new Color(96, 104, 97));
-				usernameFieldLabel.setPreferredSize(new Dimension(200,30));
-				usernameFieldLabel.addMouseListener(this);
-				
-				passwordFieldLabel = new JLabel("******");
-				passwordFieldLabel.setForeground(new Color(96, 104, 97));
-				passwordFieldLabel.setPreferredSize(new Dimension(200,30));
-				passwordFieldLabel.addMouseListener(this);
-
-				nameFieldLabel = new JLabel("Name");
-				nameFieldLabel.setForeground(new Color(96, 104, 97));
-				nameFieldLabel.setPreferredSize(new Dimension(200,30));
-				nameFieldLabel.addMouseListener(this);
-				
-				telNumberFieldLabel = new JLabel("telNo");
-				telNumberFieldLabel.setForeground(new Color(96, 104, 97));
-				telNumberFieldLabel.setPreferredSize(new Dimension(200,30));
-				telNumberFieldLabel.addMouseListener(this);
-				
-				emailFieldLabel = new JLabel("Email");
-				emailFieldLabel.setForeground(new Color(96, 104, 97));
-				emailFieldLabel.setPreferredSize(new Dimension(200,30));
-				emailFieldLabel.addMouseListener(this);
-				
-				addressNumberFieldLabel = new JLabel("Unit No");
-				addressNumberFieldLabel.setForeground(new Color(96, 104, 97));
-				addressNumberFieldLabel.setPreferredSize(new Dimension(200,30));
-				addressNumberFieldLabel.addMouseListener(this);
-				
-				addressRoadFieldLabel = new JLabel("Road");
-				addressRoadFieldLabel.setForeground(new Color(96, 104, 97));
-				addressRoadFieldLabel.setPreferredSize(new Dimension(200,30));
-				addressRoadFieldLabel.addMouseListener(this);
-				
-				addressPostcodeFieldLabel = new JLabel("Postcode");
-				addressPostcodeFieldLabel.setForeground(new Color(96, 104, 97));
-				addressPostcodeFieldLabel.setPreferredSize(new Dimension(200,30));
-				addressPostcodeFieldLabel.addMouseListener(this);
-				
-				addressStateFieldLabel = new JLabel("State");
-				addressStateFieldLabel.setForeground(new Color(96, 104, 97));
-				addressStateFieldLabel.setPreferredSize(new Dimension(200,30));
-				addressStateFieldLabel.addMouseListener(this);
-				
+			contentPane.add(textPanel,BorderLayout.LINE_START);
+			
+			
 				//TextLabel Panel Container
 				fieldLabelPanel = new JPanel();
-				fieldLabelPanel.setOpaque(false);
 				fieldLabelPanel.setPreferredSize(new Dimension(5,300));
+				
+					usernameFieldLabel = new JLabel("old username");
+					usernameFieldLabel.setForeground(new Color(96, 104, 97));
+					usernameFieldLabel.setPreferredSize(new Dimension(200,30));
+					usernameFieldLabel.addMouseListener(this);
+					
+					passwordFieldLabel = new JLabel("******");
+					passwordFieldLabel.setForeground(new Color(96, 104, 97));
+					passwordFieldLabel.setPreferredSize(new Dimension(200,30));
+					passwordFieldLabel.addMouseListener(this);
+	
+					nameFieldLabel = new JLabel("Name");
+					nameFieldLabel.setForeground(new Color(96, 104, 97));
+					nameFieldLabel.setPreferredSize(new Dimension(200,30));
+					nameFieldLabel.addMouseListener(this);
+					
+					telNumberFieldLabel = new JLabel("telNo");
+					telNumberFieldLabel.setForeground(new Color(96, 104, 97));
+					telNumberFieldLabel.setPreferredSize(new Dimension(200,30));
+					telNumberFieldLabel.addMouseListener(this);
+					
+					emailFieldLabel = new JLabel("Email");
+					emailFieldLabel.setForeground(new Color(96, 104, 97));
+					emailFieldLabel.setPreferredSize(new Dimension(200,30));
+					emailFieldLabel.addMouseListener(this);
+					
+					addressNumberFieldLabel = new JLabel("Unit No");
+					addressNumberFieldLabel.setForeground(new Color(96, 104, 97));
+					addressNumberFieldLabel.setPreferredSize(new Dimension(200,30));
+					addressNumberFieldLabel.addMouseListener(this);
+					
+					addressRoadFieldLabel = new JLabel("Road");
+					addressRoadFieldLabel.setForeground(new Color(96, 104, 97));
+					addressRoadFieldLabel.setPreferredSize(new Dimension(200,30));
+					addressRoadFieldLabel.addMouseListener(this);
+					
+					addressPostcodeFieldLabel = new JLabel("Postcode");
+					addressPostcodeFieldLabel.setForeground(new Color(96, 104, 97));
+					addressPostcodeFieldLabel.setPreferredSize(new Dimension(200,30));
+					addressPostcodeFieldLabel.addMouseListener(this);
+					
+					addressStateFieldLabel = new JLabel("State");
+					addressStateFieldLabel.setForeground(new Color(96, 104, 97));
+					addressStateFieldLabel.setPreferredSize(new Dimension(200,30));
+					addressStateFieldLabel.addMouseListener(this);
+				
+				
 				fieldLabelPanel.add(usernameFieldLabel);
 				fieldLabelPanel.add(passwordFieldLabel);
 				fieldLabelPanel.add(nameFieldLabel);
@@ -168,26 +189,27 @@ public class UserProfile extends JFrame implements ActionListener, MouseListener
 				fieldLabelPanel.add(addressPostcodeFieldLabel);
 				fieldLabelPanel.add(addressStateFieldLabel);
 				
-				 //add back button
-			     JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+			
+			
+			contentPane.add(fieldLabelPanel,BorderLayout.CENTER);
+			
+		add(contentPane,BorderLayout.CENTER);
+		
+			 //add back button
+		     northPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+		     
 			     backButton = new JButton();
-			     ImageIcon backIcon = createResizedIcon("/resources/uiSymbol/back.png",20,20);
+			     	backIcon = createResizedIcon("/resources/uiSymbol/back.png",20,20);
 			     backButton.setIcon(backIcon);
 			     backButton.addMouseListener(this);
 			     backButton.addActionListener(this);
-			     northPanel.add(backButton);
-			     add(northPanel,BorderLayout.PAGE_START);
 			     
-			     
-			contentPane = new JPanel(new BorderLayout(5,5));
-			contentPane.add(userImage,BorderLayout.PAGE_START);
-			contentPane.add(textPanel,BorderLayout.LINE_START);
-			contentPane.add(fieldLabelPanel,BorderLayout.CENTER);
-			contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));  
-			add(contentPane,BorderLayout.CENTER);
-			pack();
-			setVisible(true);
-			setResizable(false);
+			 northPanel.add(backButton);
+			 
+	     add(northPanel,BorderLayout.PAGE_START);
+		pack();
+		setVisible(true);
+		setResizable(false);
 
 	}
 
