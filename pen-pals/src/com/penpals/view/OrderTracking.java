38448c7ce9c33d7eb3209d9ff35a26f5a93734a9
p0205 <kks741224@gmail.com>
2024-01-08 11:22:12 +0800

    order tracking gui renew

--- pen-pals/src/com/penpals/view/OrderTracking.java
+++ pen-pals/src/com/penpals/view/OrderTracking.java
@@ -36,10 +36,55 @@ import com.penpals.model.Order;
 public class OrderTracking extends JFrame implements MouseListener, ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JButton completeOrderButton;
-	private JButton backButton;
+	
+	
 	private Order order;
+	
+	//south panel
+	private JPanel southPanel;
+		private JLabel completeLabel;
+		private JButton completeOrderButton;
+	
+	//north panel
+	private JPanel northPanel;
+	
+		private JButton backButton;
+			private ImageIcon backIcon;
+			
+		
+	private JScrollPane scrollableBrowseArea;		
+	
+		//center panel
+		private JPanel contentPane;
+		
+			//container consist of status and address
+			private JPanel container;
+			
+				private JPanel statusPanel;
+					private JLabel statusLabel;
+				
+				private JPanel addressPanel;
+					private JLabel addressLabel;
+				
+					private JPanel productPanels;
+						private JPanel productPanel ;
+							private JLabel imageLabel;
+								private ImageIcon productImage; 
+								
+							private JLabel nameLabel;
+							private JPanel qtyPricePanel;
+								private JLabel quantityLabel;
+								private JLabel priceLabel;
+												
+			private JPanel totalPanel ;
+				private JLabel totalPriceLabel;
+				private JLabel totalLabel;
+			
+			private JPanel datePanel;
+				private JLabel dateLabel;
+				private JLabel orderDateLabel;
+						
+	
 
 
 
@@ -49,207 +94,221 @@ public class OrderTracking extends JFrame implements MouseListener, ActionListen
 	 */
 	public OrderTracking(Order order) {   
 		this.order = order;
-		initializeComponent(order);
+		init(order);
 	}
 	
 
 
-	private void initializeComponent(Order order) 
+	private void init(Order order) 
 	{
+		//frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setLayout(new BorderLayout());
-		 Dimension minimumSize = new Dimension(900,700); 
-			//set frame min size
-	     setMinimumSize(minimumSize);
-	     
-			//status JLabel
-			String statusString;
-			if(order.getOrderIsDelivered())
+	    setMinimumSize(new Dimension(900,700));
+		     
+		    
+		    contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPane.setLayout(new BorderLayout(0, 0));
+		     
+			scrollableBrowseArea = new JScrollPane(contentPane);
+		    scrollableBrowseArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		    
+	    getContentPane().add(scrollableBrowseArea, BorderLayout.CENTER);  
+	    
+			    container = new JPanel();
+				container.setBorder(new EmptyBorder(10, 10, 10, 10));  
+				BoxLayout contentBox = new BoxLayout(container, BoxLayout.Y_AXIS); 
+				// to set the box layout 
+				container.setLayout(contentBox);
+				
+					//status panel
+					statusPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+					
+						statusLabel = new JLabel();
+						//status JLabel
+						String statusString;
+						if(order.getOrderIsDelivered())
+						{
+							statusString = "Status: In Delivery";
+						}
+						else
+						{
+							statusString = "Status: Delivered";
+						}
+						statusLabel.setText(statusString);
+						statusLabel.setHorizontalAlignment(JLabel.LEFT);
+					
+					statusPanel.add(statusLabel);
+					statusPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+					
+					
+					//address panel
+					addressPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+					
+						addressLabel = new JLabel();
+						
+							int number = order.getOrderCustomer().getCustomerAddress().getNumber();
+							int postcode = order.getOrderCustomer().getCustomerAddress().getPostcode();
+							String state = order.getOrderCustomer().getCustomerAddress().getState();
+							String road = order.getOrderCustomer().getCustomerAddress().getRoad();
+							String address = String.valueOf(number) + ", " + road + ", " + String.valueOf(postcode)+ state;
+							address = "Address : " + address;
+				
+						addressLabel.setText(address);
+					
+					addressPanel.add(addressLabel);
+					addressPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+					
+				
+				 
+					//Product List panel
+				 	//iterate through cartitem
+					 productPanels = new JPanel(new GridLayout(0,1,0,0));
+					 List<CartItem> cartItems = order.getOrderCartItems();
+				 
+					for(CartItem cartItem : cartItems) 
+					{
+							productPanel = new JPanel(new BorderLayout(5,0));
+			//					//product panel for each Product
+								//image JLabel
+								imageLabel = new JLabel("");
+								
+						        	productImage = createResizedIcon(cartItem.getCartItemProduct().getProductImageURL(), 100,100); 
+						        	
+						        imageLabel.setIcon(productImage);
+						        imageLabel.setVerticalAlignment(JLabel.CENTER);
+						        
+						   
+						        
+								//product name JLabel
+						        nameLabel = new JLabel();
+						        nameLabel.setText(cartItem.getCartItemProduct().getProductName());
+						        nameLabel.setVerticalAlignment(JLabel.CENTER);
+						  
+					        
+						        qtyPricePanel = new JPanel(new BorderLayout());
+									//quantity JLabel
+							        quantityLabel = new JLabel();
+							        
+								        int quantity = cartItem.getCartItemQuantity();
+								        String quantityString = String.valueOf(quantity);
+								        quantityString = " x " + quantityString ;
+								        
+								    quantityLabel.setText(quantityString);
+							        
+									//total Price for each product JLabel
+							        priceLabel = new JLabel();
+							        
+								        double productPrice = cartItem.getCartItemProduct().getProductPrice();
+								        double cartItemPrice = productPrice * quantity;
+								        // Format the number to 2 decimal price
+								        DecimalFormat df = new DecimalFormat("#.00");
+								        String cartItemPriceString = df.format(cartItemPrice);
+					
+								        cartItemPriceString = "RM " + cartItemPriceString;
+							        
+							        priceLabel.setText(cartItemPriceString);
+						        
+						     	qtyPricePanel.add(quantityLabel,BorderLayout.CENTER);
+						     	qtyPricePanel.add(priceLabel,BorderLayout.SOUTH);
+						     	
+						     
+						     productPanel.add(imageLabel,BorderLayout.LINE_START);
+						     productPanel.add(nameLabel,BorderLayout.CENTER);
+						     productPanel.add(qtyPricePanel,BorderLayout.LINE_END);
+						     productPanel.setPreferredSize(new Dimension(200,100));
+						     productPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+					     productPanels.add(productPanel);
+					     
+					}
+					 container.add(statusPanel);
+					 container.add(Box.createRigidArea(new Dimension(0,5)));
+					 container.add(addressPanel);
+					 container.add(Box.createRigidArea(new Dimension(0,5)));
+					 container.add(productPanels);
+					
+				    //total price Panel
+					totalPanel = new JPanel(new BorderLayout());
+					
+						//total price JLabel 
+					     totalPriceLabel = new JLabel();
+						   
+						     double price = order.getOrderTotal();
+						     String totalPriceString = String.valueOf(price);
+						     totalPriceString  = "RM " + totalPriceString;
+						     
+					     totalPriceLabel.setText(totalPriceString);
+					     totalPriceLabel.setVerticalAlignment(JLabel.BOTTOM);
+					
+						 totalLabel = new JLabel("Amount Paid");
+						 totalLabel.setVerticalAlignment(JLabel.BOTTOM);
+			
+					totalPanel.add(totalLabel,BorderLayout.LINE_START);
+					totalPanel.add(totalPriceLabel,BorderLayout.LINE_END);
+					
+					//date panel
+					datePanel = new JPanel(new BorderLayout());
+					
+						dateLabel = new JLabel("Order Date");
+						dateLabel.setVerticalAlignment(JLabel.TOP);
+						
+					    //Order date JLabel
+					     Date date = order.getOrderDate();
+					     String orderDate = date.toString();
+					     orderDateLabel = new JLabel();
+					     orderDateLabel.setVerticalAlignment(JLabel.TOP);
+					     orderDateLabel.setText(orderDate);
+					     
+					     
+					datePanel.add(dateLabel,BorderLayout.LINE_START);
+					datePanel.add(orderDateLabel,BorderLayout.LINE_END);
+	
+			contentPane.add(container,BorderLayout.NORTH);
+		    contentPane.add(totalPanel,BorderLayout.CENTER);
+		    contentPane.add(datePanel,BorderLayout.SOUTH);
+			   
+			 //add back button
+		     northPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+		     	backButton = new JButton();
+		     		backIcon = createResizedIcon("/resources/uiSymbol/back.png",20,20);
+			     backButton.setIcon(backIcon);
+			     backButton.addMouseListener(this);
+			     backButton.addActionListener(this);
+		     northPanel.add(backButton);
+	     add(northPanel,BorderLayout.PAGE_START);
+	    
+			//complete order button add to frame (exist only if order haven't completed)
+			if(!order.getIsCompleted())
 			{
-				statusString = "Status: In Delivery";
+					southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+					
+						completeOrderButton = new JButton();
+						completeOrderButton.setText("Complete Order");	
+						completeOrderButton.addActionListener(this);
+						completeOrderButton.addMouseListener(this);
+						
+					southPanel.add(completeOrderButton);
+					
+				add(southPanel,BorderLayout.SOUTH);
 			}
 			else
 			{
-				statusString = "Status: Delivered";
+					southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+					
+						completeLabel = new JLabel();
+						completeLabel.setText("The order is completed.");	
+						completeLabel.setHorizontalAlignment(JLabel.CENTER);
+						
+					southPanel.add(completeLabel);
+				add(southPanel,BorderLayout.SOUTH);
 			}
-			JLabel statusLabel = new JLabel();
-			statusLabel.setText(statusString);
-			statusLabel.setHorizontalAlignment(JLabel.LEFT);
-			JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-			statusPanel.add(statusLabel);
-			statusPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-			
-			
-			//address JLanel
-			int number = order.getOrderCustomer().getCustomerAddress().getNumber();
-			int postcode = order.getOrderCustomer().getCustomerAddress().getPostcode();
-			String state = order.getOrderCustomer().getCustomerAddress().getState();
-			String road = order.getOrderCustomer().getCustomerAddress().getRoad();
-			
-			String address = String.valueOf(number) + ", " + road + ", " + String.valueOf(postcode)+ state;
-			address = "Address : " + address;
-			JLabel addressLabel = new JLabel();
-			addressLabel.setText(address);
-			JPanel addressPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-			addressPanel.add(addressLabel);
-			addressPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-			
-			
-			JPanel container = new JPanel();
-			container.setBorder(new EmptyBorder(10, 10, 10, 10));  
-		
-		 BoxLayout contentBox = new BoxLayout(container, BoxLayout.Y_AXIS); 
-		 // to set the box layout 
-		 container.setLayout(contentBox);
-		 container.add(statusPanel);
-		 container.add(Box.createRigidArea(new Dimension(0,5)));
-		 container.add(addressPanel);
-		 container.add(Box.createRigidArea(new Dimension(0,5)));
-		 
-			//Product List panel
-		 	//iterate through cartitem
-		 JPanel productPanels = new JPanel(new GridLayout(0,1,0,0));
-		 List<CartItem> cartItems = order.getOrderCartItems();
-		 
-			for(CartItem cartItem : cartItems) // delete this and change to iteration of list
-			{
-				
-//					//product panel for each Product
-					//image JLabel
-					JLabel imageLabel = new JLabel("");
-			        ImageIcon productImage = createResizedIcon(cartItem.getCartItemProduct().getProductImageURL(), 100,100); 
-			        imageLabel.setIcon(productImage);
-			        imageLabel.setVerticalAlignment(JLabel.CENTER);
-			        
-			   
-			        
-					//product name JLabel
-			        JLabel nameLabel = new JLabel();
-			        nameLabel.setText(cartItem.getCartItemProduct().getProductName());
-			        nameLabel.setVerticalAlignment(JLabel.CENTER);
-			  
-		        
-					//quantity JLabel
-			        //int quantity = cartItem.getQuantity();
-			        int quantity = cartItem.getCartItemQuantity();
-			        String quantityString = String.valueOf(quantity);
-			        quantityString = " x " + quantityString ;
-			        JLabel quantityLabel = new JLabel();
-			        quantityLabel.setText(quantityString);
-			        
-					//total Price JLabel
-			        double productPrice = cartItem.getCartItemProduct().getProductPrice();
-			        double cartItemPrice = productPrice * quantity;
-			        // Format the number to 2 decimal price
-			        DecimalFormat df = new DecimalFormat("#.00");
-			        String cartItemPriceString = df.format(cartItemPrice);
-
-			        cartItemPriceString = "RM " + cartItemPriceString;
-			        
-			        
-			        JLabel priceLabel = new JLabel();
-			        priceLabel.setText(cartItemPriceString);
-			        
-			     JPanel productPanel = new JPanel(new BorderLayout(5,0));
-			     productPanel.add(imageLabel,BorderLayout.LINE_START);
-			     productPanel.add(nameLabel,BorderLayout.CENTER);
-			     
-			     	JPanel qtyPricePanel = new JPanel(new BorderLayout());
-			     	qtyPricePanel.add(quantityLabel,BorderLayout.CENTER);
-			     	qtyPricePanel.add(priceLabel,BorderLayout.SOUTH);
-			     	
-	
-			     productPanel.add(qtyPricePanel,BorderLayout.LINE_END);
-			     productPanel.setPreferredSize(new Dimension(200,100));
-			     productPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-			     productPanels.add(productPanel);
-			     
-			}
-			container.add(productPanels);
-			
-		     //total price JLabel 
-		     //double price = order.getOrderTotal();
-		     double price = order.getOrderTotal();
-		     String totalPriceString = String.valueOf(price);
-		     totalPriceString  = "RM " + totalPriceString;
-		     JLabel totalPriceLabel = new JLabel();
-		     totalPriceLabel.setText(totalPriceString);
-		     totalPriceLabel.setVerticalAlignment(JLabel.BOTTOM);
-		     
-		     //Order date JLabel
-		     Date date = order.getOrderDate();
-		     String orderDate = date.toString();
-		     JLabel orderDateLabel = new JLabel();
-		     orderDateLabel.setVerticalAlignment(JLabel.TOP);
-		     orderDateLabel.setText(orderDate);
-		     
-		    //total price Panel
-			JPanel totalPanel = new JPanel(new BorderLayout());
-			
-				JLabel totalLabel = new JLabel("Amount Paid");
-				totalLabel.setVerticalAlignment(JLabel.BOTTOM);
-	
-			totalPanel.add(totalLabel,BorderLayout.LINE_START);
-			totalPanel.add(totalPriceLabel,BorderLayout.LINE_END);
-			
-			//date panel
-			JPanel datePanel = new JPanel(new BorderLayout());
-			
-				JLabel dateLabel = new JLabel("Order Date");
-				dateLabel.setVerticalAlignment(JLabel.TOP);
-				
-			datePanel.add(dateLabel,BorderLayout.LINE_START);
-			datePanel.add(orderDateLabel,BorderLayout.LINE_END);
-
-
-	 //add back button
-     JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-     backButton = new JButton();
-     ImageIcon backIcon = createResizedIcon("/resources/uiSymbol/back.png",20,20);
-     backButton.setIcon(backIcon);
-     backButton.addMouseListener(this);
-     backButton.addActionListener(this);
-     northPanel.add(backButton);
-     add(northPanel,BorderLayout.PAGE_START);
+		    
+	    setTitle("Penpals ");
+	    setVisible(true);
     
-		//complete order button add to frame (exist only if order haven't completed)
-		if(!order.getIsCompleted())
-		{
-			JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-			completeOrderButton = new JButton();
-			completeOrderButton.setText("Complete Order");	
-			completeOrderButton.addActionListener(this);
-			completeOrderButton.addMouseListener(this);
-			buttonPanel.add(completeOrderButton);
-			add(buttonPanel,BorderLayout.SOUTH);
-		}
-		else
-		{
-			JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-			JLabel completeLabel = new JLabel();
-			completeLabel.setText("The order is completed.");	
-			completeLabel.setHorizontalAlignment(JLabel.CENTER);
-			southPanel.add(completeLabel);
-			add(southPanel,BorderLayout.SOUTH);
-		}
-	
-	contentPane = new JPanel();
-	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	contentPane.setLayout(new BorderLayout(0, 0));
-     
-	JScrollPane scrollableBrowseArea = new JScrollPane(contentPane);
-    scrollableBrowseArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-    contentPane.add(container,BorderLayout.NORTH);
-    contentPane.add(totalPanel,BorderLayout.CENTER);
-    contentPane.add(datePanel,BorderLayout.SOUTH);
-    getContentPane().add(scrollableBrowseArea, BorderLayout.CENTER);  
-    setTitle("Order Details");
-    setVisible(true);
-    
-	
-	
 	}
 
 	@Override
