21a620afb606518506d9c48fad066347c5fb1d2d
SitiSafiahSFG <b032210136@student.utem.edu.my>
2024-01-24 19:40:58 +0800

    message

--- pen-pals/src/App.java
+++ pen-pals/src/App.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import com.penpals.controller.CustomerController;
 import com.penpals.model.Customer;
-import com.penpals.view.Login;
+
 import com.penpals.db.MyDatabase;
 import com.penpals.view.CartGui;
 import com.penpals.view.LoginGui;
@@ -16,45 +16,7 @@ public class App {
     	LoginGui frame = new LoginGui();
         frame.setVisible(true);
         
-        /**MyDatabase.doConnection();
-        Scanner scanner = new Scanner(System.in);
-        String username;
-        String password;
-
-        System.out.println("Pen Pals");
-        System.out.println("========");
-
-        System.out.println("Login Page");
-        System.out.println("==========");
-        System.out.println("Username: ");
-        username = scanner.nextLine();
-        System.out.println("Password: ");
-        password = scanner.nextLine();
-        System.out.println("==========");
-
-        CustomerController customerController = new CustomerController();
-        customerController.connectToDatabase();
-        Customer customer = customerController.getCustomerDetailbyUsernamePassword(username, password);
-
-        if (customer.getCustomerId() != 0)
-        {
-            System.out.println("Welcome " + customer.getCustomerName());
-            System.out.println("==========");
-            System.out.println("Customer Details");
-            System.out.println("==========");
-            System.out.println("Name: " + customer.getCustomerName());
-            System.out.println("Email: " + customer.getCustomerEmail());
-            System.out.println("Tel Number: " + customer.getCustomerTelNumber());
-            System.out.println("Address: " + customer.getCustomerAddress().getNumber() + " " + customer.getCustomerAddress().getRoad() + ", " + customer.getCustomerAddress().getPostcode() + ", " + customer.getCustomerAddress().getState());
-            System.out.println("==========");
-        }
-        else
-        {
-            System.out.println("Invalid username or password");
-        }
-
-        scanner.close();**/
-        Login login = new Login();
-        login.setVisible(true);
+        
+       
     }
 }
