8e4e9b8f213b896732a04a475c34a172249cace0
p0205 <kks741224@gmail.com>
2024-01-25 09:56:40 +0800

    /

--- pen-pals/src/App.java
+++ pen-pals/src/App.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-//import com.penpals.db.MyDatabase;
+
 import java.util.Scanner;
 import com.penpals.controller.CustomerController;
 import com.penpals.db.MyDatabase;
@@ -12,7 +11,6 @@ import com.penpals.model.*;
 import java.util.List;
 =======
 import com.penpals.view.LoginGui;
->>>>>>> branch 'cartBackend' of https://github.com/YIsheng2002/pen-pals.git
 
 public class App {
     public static void main(String[] args) throws Exception {
