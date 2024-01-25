fde1a5b74368943d6abf6a87c71e2530246dd206
p0205 <kks741224@gmail.com>
2024-01-25 11:13:59 +0800

    ,

--- pen-pals/src/App.java
+++ pen-pals/src/App.java
@@ -1,19 +1,7 @@
-
-import java.util.Scanner;
-import com.penpals.controller.CustomerController;
-import com.penpals.db.MyDatabase;
-import com.penpals.model.Customer;
-import com.penpals.db.MyDatabase;
-import com.penpals.controller.*;
-import com.penpals.view.*;
-import com.penpals.model.*;
-
-import java.util.List;
-=======
 import com.penpals.view.LoginGui;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+	public static void main(String args[]) throws Exception {
     	LoginGui frame = new LoginGui();
         frame.setVisible(true);
     }
